4eeaf764905f8a6f3410e115fef5a020b76480a6
adisito <adisito@gmail.com>
2016-08-24 08:16:53 +0700

    start 24 August 16

---
+++ app/src/main/java/kmutnb/ited/adisito/robotarm/MainActivity.java
@@ -0,0 +1,13 @@
+package kmutnb.ited.adisito.robotarm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
