0978328da4d3906e4c3ca62e15d0ca44bc468786
adisito <adisito@gmail.com>
2016-08-24 08:58:20 +0700

    start 24 August 16 connect

--- app/src/main/java/kmutnb/ited/adisito/robotarm/MainActivity.java
+++ app/src/main/java/kmutnb/ited/adisito/robotarm/MainActivity.java
@@ -2,12 +2,55 @@ package kmutnb.ited.adisito.robotarm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.RequestBody;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Bind Wiget
+        textView = (TextView) findViewById(R.id.textView);
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+
+        // Seek bar controller
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textView.setText(Integer.toString(progress*10));
+                uploadvalueToDweet(progress * 10);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }// Main Method
+
+    private void uploadvalueToDweet(int i) {
+
+        String urlDweet = "https://dweet.io/dweet/for/adisito?servo1=" + Integer.toString(i);
+        OkHttpClient okHttpClient = new OkHttpClient();
+        RequestBody requestBody = new FormEncodingBuilder()
+                .add("Test", "test")
+                .build();
+
+    }// Upload
+}// Main clashh
